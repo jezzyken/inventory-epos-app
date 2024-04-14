21bc6afd1c7d1a5d0b94135cb5cc3210d3f667df
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-04-14 07:57:29 -0600

    add error middleware

--- server/serverRoutes.js
+++ server/serverRoutes.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/error.controller');
 require('dotenv').config({path: './.env'});
 const API_VERSION = process.env.API_VERSION;
 const SERVER = process.env.SERVER;
-
-
-
 
 const serverRoutes = (app) => {
   const routeFiles = fs.readdirSync(`./routes/${API_VERSION}`);
@@ -18,6 +17,7 @@ const serverRoutes = (app) => {
         .toLowerCase();
       const routeModule = require(`./routes/${API_VERSION}/${routeFile}`);
       app.use(`/api/${API_VERSION}/${SERVER}/${routePath}`, routeModule);
+      app.use(globalErrorHandler);
     }
   });
 };
