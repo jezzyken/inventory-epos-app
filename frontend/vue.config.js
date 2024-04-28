5704341f529767562be1e03e583ebd9d71deb7a7
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-04-28 11:17:01 -0600

    production updates

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = "D'tio Admin";
+      return args;
+    });
+  }
 })
