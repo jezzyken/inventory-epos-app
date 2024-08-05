0a1a76863e659723f3ee72c6f63ca90a7b9b85d8
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-08-05 13:19:24 +0800

    updates  -update node env  -add public to .gitignore

--- frontend/vue2/src/config/index.js
+++ frontend/vue2/src/config/index.js
@@ -1,7 +1,14 @@
 let API_VERSION = "v1"; //default
 let SERVER = "node";
 
+let url;
+
+if (process.env.NODE_ENV === "production") {
+  url = `/api/${API_VERSION}/${SERVER}`;
+} else {
+  url = `http://localhost:3001/api/${API_VERSION}/${SERVER}`;
+}
+
 module.exports = {
-  // url: `http://localhost:3001/api/${API_VERSION}/${SERVER}`,
-  url: `/api/${API_VERSION}/${SERVER}`,
+  url,
 };
