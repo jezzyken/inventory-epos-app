c50db00e6edeef1d866374c5c1afb7c099127691
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-08-05 12:23:21 +0800

    update config var

--- frontend/vue2/src/config/index.js
+++ frontend/vue2/src/config/index.js
@@ -1,8 +1,7 @@
-
-let API_VERSION = 'v1' //default
-let SERVER = 'node'
+let API_VERSION = "v1"; //default
+let SERVER = "node";
 
 module.exports = {
-  url: `http://localhost:3001/api/${API_VERSION}/${SERVER}`,
-  // url: `/api/${API_VERSION}/${SERVER}`,
+  // url: `http://localhost:3001/api/${API_VERSION}/${SERVER}`,
+  url: `/api/${API_VERSION}/${SERVER}`,
 };
